f5aae1fe8bd369e3c360bcf60341982cd0eb7dba
Cole Rudnick <rudnickcole@Gmail.com>
2015-09-24 18:15:44 -0700

    a
    a

--- src/GH/Main.java
+++ src/GH/Main.java
@@ -13,14 +13,14 @@ public class Main {
 	GameFrame gf;
 	Game g;
 	AudioClip sound = JApplet.newAudioClip(getClass().getResource("Nirvana_-_Smells_Like_Teen_Spirit.wav"));
-	
+
 	public static void main(String[] args) {
 		Buttons b = new Buttons(Buttons.BLUE);
 		b.loadUp();
 		Main m = new Main();
 		m.init();
 		m.run();
-		
+
 	}
 
 	private void run() {
@@ -28,16 +28,19 @@ public class Main {
 		Thread t2 = new Thread(gf);
 		t1.start();
 		t2.start();
-try {
-	t2.sleep(6666666);
-} catch (InterruptedException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
+		try {
+			t2.sleep(6666666);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private void init() {
-		JOptionPane.showMessageDialog(null, "HI!!! Thanks for playing my game. If you have already played Guitar Hero than you should catch on quick. q=green, w=red, e=yellow, r=blue. No need to strum though. The score is displayed at the top of the screen! Thanks for playing!");
+		JOptionPane.showMessageDialog(null,
+				"HI!!! Thanks for playing my game. If you have already played Guitar Hero than you should catch on quick."
+						+ "\nq=green, w=red, e=yellow, r=blue. No need to strum though. The score is displayed at the top of the screen!"
+						+ "\n                                                          Thanks for playing!");
 		g = new Game();
 		gf = new GameFrame(g);
 		gf.setSize(1000, 500);
